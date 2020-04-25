9bd2c9228a27e949c163cbe291ccf8969cb6549c
Gilson <gilson.costa@dce.ufpb.br>
2020-04-25 09:45:08 -0300

    Inserindo departamentos

--- src/main/java/com/example/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/example/curso/boot/web/controller/DepartamentoController.java
@@ -1,15 +1,23 @@
 package com.example.curso.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.curso.boot.domain.Departamento;
+import com.example.curso.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+	
+	@Autowired
+	private DepartamentoService service;
 
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
@@ -18,4 +26,9 @@ public class DepartamentoController {
 		return "/departamento/lista";
 	}
 	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		this.service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
 }
