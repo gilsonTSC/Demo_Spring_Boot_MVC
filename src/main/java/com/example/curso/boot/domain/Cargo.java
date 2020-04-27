0f216d9f778524cdd6ada9cdd2a9d76b12b936db
Gilson <gilson.costa@dce.ufpb.br>
2020-04-27 18:06:10 -0300

    Validando formulário de cargo e departamento

--- src/main/java/com/example/curso/boot/domain/Cargo.java
+++ src/main/java/com/example/curso/boot/domain/Cargo.java
@@ -8,16 +8,22 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @SuppressWarnings("serial")
 @Entity
 @Table(name= "CARGOS")
 public class Cargo extends AbstractEntity<Long>{
 
+	@NotBlank(message = "Informe um nome.")
+	@Size(min = 3, max = 60, message = "O nome do departamento deve ter entre {min} e {max} caracteres.")
 	@Column(name= "nome", nullable = false, unique = true, length = 60)
 	private String nome;
 	
 	@ManyToOne
+	@NotNull(message = "Selecione o departamento relativo ao cargo.")
 	@JoinColumn(name = "id_departamento_fk")
 	private Departamento departamento;
 	
